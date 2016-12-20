af8a104149b60813ba929872f27befb776e031ad
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-12-20 16:43:57 -0500

    updated title for Home route

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -10,11 +10,12 @@
 import React from 'react';
 import Home from './Home';
 import fetch from '../../core/fetch';
+import { appName } from '../../config';
 
 export const path = '/';
 export const action = async (state) => {
   const response = await fetch('/graphql?query={news{title,link,contentSnippet}}');
   const { data } = await response.json();
-  state.context.onSetTitle('React.js Starter Kit');
+  state.context.onSetTitle(appName);
   return <Home news={data.news} />;
 };
